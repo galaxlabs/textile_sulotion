c4abedcb3dc1454c24485cedc60c34bdb8d79314
galaxlabs <galaxylab2020@gmail.com>
2025-11-19 14:53:47 +0000

    init Textile App

---
+++ textile_sulotion/textile_sulotion/doctype/order_size_breakup/order_size_breakup.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Galaxy labs and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class OrderSizeBreakup(Document):
+	pass
